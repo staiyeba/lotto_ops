eefe1a80ea138a381d1abc04cee7d6da599f057b
taiyeba <staiyeba@gmail.com>
2018-06-26 22:59:05 +0200

    html beautify: adding files for appending to csv html

---
+++ searchtable.js
@@ -0,0 +1,22 @@
+
+<script>
+  function searchFunc() {
+      var input, filter, table, tr, td, i;
+      input = document.getElementById("input");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
+      for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
+          if (td) {
+              if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+              } else {
+                tr[i].style.display = "none";
+              }
+          }
+      }
+  }
+</script>
+</body>
+</html>
